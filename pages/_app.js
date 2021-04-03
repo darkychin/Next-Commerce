f9f3f80dbe0fde315a03f5868e03b0a8457e94fc
Darky <darkychin95@gmail.com>
2021-04-03 23:26:07 +0800

    Update _app.js
    Update to use antd.less

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+import "../styles/antd.less";
+// import '../styles/globals.css'；
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
